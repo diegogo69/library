5163c1074cad8ddbe3c92c6427cf479c67f6f46f
diegogo69 <153313506+diegogo69@users.noreply.github.com>
2024-09-23 13:36:10 -0400

    REFACTOR: svgIcon object manages dynamic svg els
    Create svgIcon object to group functionality to create dynamically
    Svg elements to be added in the status column for each row

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-const log = console.log;
-
 // Library array
 let myLibrary = [];
 
@@ -17,10 +15,30 @@ Book.prototype.index = function() {
     return myLibrary.indexOf(this);
 }
 
+// Add book to library
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
+// Create book object and add it to library
+function addBook(title, author, pages, read) {
+    let book = new Book(title, author, pages, read);
+    addBookToLibrary(book);
+    return book;
+}
+
+const initialBooks = [
+    ["Eloquent JavaScript", "Marijn Haverbeke", 435, false, true],
+    ["Programming Logic and Design", "Joyce Farrel", 717, true, true],
+    ["Think Like a Programmer", "Anton Spraul", 256, false, true]
+]
+
+// Add initial set of books to library array
+for (let book of initialBooks) {
+    addBook(...book)
+}
+
+// Toggle read status
 function toggleRead(book, event) {
     if (book.read === true) {
         book.read = false;
@@ -30,12 +48,14 @@ function toggleRead(book, event) {
     event.target.closest('svg').classList.toggle('read');
 }
 
+// Remove book from library
 function removeBook(book, event) {
     let row = document.querySelector(`tr[data-index="${book.index()}"]`);
     row.parentNode.removeChild(row);    
     myLibrary[book.index()] = null;
 }
 
+// Toggle favorite
 function toggleFav(book, event){
     if (book.favorite === true) {
         book.favorite = false;
@@ -45,14 +65,8 @@ function toggleFav(book, event){
     event.target.closest('svg').classList.toggle('favorite')
 }
 
-// create table for books
-const table = document.querySelector('tbody');
-function createTable() {
-    for (let book of myLibrary) {
-        createBookRow(book);
-    }
-}
 // Create book row in the table
+const table = document.querySelector('tbody');
 function createBookRow(book) {
     let row = document.createElement('tr');
     row.setAttribute('data-index', `${book.index()}`);
@@ -62,7 +76,7 @@ function createBookRow(book) {
             continue
         }
         else if (prop == "read") {
-            let readIcon = svgRead();
+            let readIcon = svgIcon.read();
             cell.classList.add('status');
             cell.appendChild(readIcon);
             
@@ -71,12 +85,12 @@ function createBookRow(book) {
             };
         }
         else if (prop == 'favorite') {
-            let favIcon = svgFav();
+            let favIcon = svgIcon.favorite();
             if (book[prop]) {
                 favIcon.classList.toggle('favorite')
             }
             row.querySelector('.status').appendChild(favIcon);
-            let removeIcon = svgRemove();
+            let removeIcon = svgIcon.remove();
             row.querySelector('.status').appendChild(removeIcon);
             continue;
         }
@@ -92,63 +106,53 @@ function createBookRow(book) {
     table.appendChild(row);
 }
 
-function svgRead() {
-    let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-    svg.setAttribute('viewBox', "0 0 24 24");
-    svg.classList.add('iconRead');
-    svg.innerHTML = `
-        <title>Read</title>
-        <path d="M0.41,13.41L6,19L7.41,17.58L1.83,12M22.24,5.58L11.66,16.17L7.5,12L6.07,13.41L11.66,19L23.66,7M18,7L16.59,5.58L10.24,11.93L11.66,13.34L18,7Z" />
-        `;
-
-    svg.addEventListener('click', event => toggleRead(myLibrary[+event.target.closest('tr').dataset['index']], event))
-    return svg;
+// Fill table body with books
+function createTable() {
+    for (let book of myLibrary) {
+        createBookRow(book);
+    }
 }
 
-function svgRemove() {
-    let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-    svg.setAttribute('viewBox', "0 0 24 24");
-    svg.classList.add('iconRemove');
-    svg.innerHTML = `
-        <title>Remove</title>
-        <path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" />
-        `;
-    svg.addEventListener('click', event => removeBook(myLibrary[+event.target.closest('tr').dataset['index']], event))
-    return svg;
+// create object with fucntions for creating svg icon elements
+svgIcon = {
+    read: function() {
+        let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+        svg.setAttribute('viewBox', "0 0 24 24");
+        svg.classList.add('iconRead');
+        svg.innerHTML = `
+            <title>Read</title>
+            <path d="M0.41,13.41L6,19L7.41,17.58L1.83,12M22.24,5.58L11.66,16.17L7.5,12L6.07,13.41L11.66,19L23.66,7M18,7L16.59,5.58L10.24,11.93L11.66,13.34L18,7Z" />
+            `;
+    
+        svg.addEventListener('click', event => toggleRead(myLibrary[+event.target.closest('tr').dataset['index']], event))
+        return svg;
+    },
+    remove: function() {
+        let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+        svg.setAttribute('viewBox', "0 0 24 24");
+        svg.classList.add('iconRemove');
+        svg.innerHTML = `
+            <title>Remove</title>
+            <path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" />
+            `;
+        svg.addEventListener('click', event => removeBook(myLibrary[+event.target.closest('tr').dataset['index']], event))
+        return svg;
+    },
+    favorite: function() {
+        let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+        svg.setAttribute('viewBox', "0 0 24 24");
+        svg.classList.add('iconFav');
+        svg.innerHTML = `
+            <title>Favorite</title>
+            <path d="M12,21.35L10.55,20.03C5.4,15.36 2,12.27 2,8.5C2,5.41 4.42,3 7.5,3C9.24,3 10.91,3.81 12,5.08C13.09,3.81 14.76,3 16.5,3C19.58,3 22,5.41 22,8.5C22,12.27 18.6,15.36 13.45,20.03L12,21.35Z" />
+            `;
+        svg.addEventListener('click', event => toggleFav(myLibrary[+event.target.closest('tr').dataset['index']], event))
+        return svg;
+    },
 }
-
-function svgFav() {
-    let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-    svg.setAttribute('viewBox', "0 0 24 24");
-    svg.classList.add('iconFav');
-    svg.innerHTML = `
-        <title>Favorite</title>
-        <path d="M12,21.35L10.55,20.03C5.4,15.36 2,12.27 2,8.5C2,5.41 4.42,3 7.5,3C9.24,3 10.91,3.81 12,5.08C13.09,3.81 14.76,3 16.5,3C19.58,3 22,5.41 22,8.5C22,12.27 18.6,15.36 13.45,20.03L12,21.35Z" />
-        `;
-    svg.addEventListener('click', event => toggleFav(myLibrary[+event.target.closest('tr').dataset['index']], event))
-    return svg;
-}
-
-function addBook(title, author, pages, read) {
-    let book = new Book(title, author, pages, read);
-    addBookToLibrary(book);
-    return book;
-}
-
-const initialBooks = [
-    ["Eloquent JavaScript", "Marijn Haverbeke", 435, false, true],
-    ["Programming Logic and Design", "Joyce Farrel", 717, true, true],
-    ["Think Like a Programmer", "Anton Spraul", 256, false, true]
-]
-
-// Add initial set of books to library
-for (let book of initialBooks) {
-    addBook(...book)
-}
-createTable();
 
 // reference form
 const form = document.querySelector('form');
@@ -174,3 +178,6 @@ const addBookBtn = document.querySelector('main > button');
 addBookBtn.addEventListener('click', event => {
     dialog.showModal()
 })
+
+// Create actual page book's table
+createTable();
